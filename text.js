2d2e6370776da69f212125b133f07fd178cac11f
adewale-adeyelu <adewaleadeyelu17@gmail.comm>
2024-12-27 15:40:40 +0100

    zedcrest

---
+++ text.js
@@ -0,0 +1,9 @@
+// const container = document.getElementById('content');
+// window.addEventListener('wheel', (Event)) = {
+//     if (Event.deltaY > 0) {
+//         document.write('Invest')
+//         container.style.backgroundColor = 'orange';
+//     } else if {
+//         document.write('Grow')
+//     }
+// }
